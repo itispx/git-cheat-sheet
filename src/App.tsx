7e93dbc7e5cbe8a146172323bd8543ac5e81c77d
urboypx <guilhermeciotapx@outlook.com>
2022-10-06 19:06:17 -0300

    git push

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App() {
       <Section title="creating a new repository">
         to initialize a git repository inside a folder run <br />
         <code>git init</code> <br />
-        and you're all set
+        to connect your local repository with a remote repository run <br />
+        <code>git remote add origin &lt;remote-repo-url&gt;</code>
       </Section>
 
       <Section title="cloning a repository">
@@ -33,6 +34,21 @@ function App() {
         {/* TODO unstage  */}
         to commit the files you just staged to your local repository run <br />
         <code>git commit -m "descriptive commit message"</code>
+      </Section>
+
+      <Section title="push">
+        to push your code to the remote repository you need to run <br />
+        <code>git push -u origin master</code> <br />
+        this assumes that you're pushing to the master branch the next time you
+        push your code you only need to run <br />
+        <code>git push</code> <br />
+        <br />
+        the <code>-u</code> tag sets the default branch to whatever you choose
+        <br />
+        if you want to push your code to a different branch you can do <br />
+        <code>git push -u origin &lt;branch&gt;</code>
+        <br />
+        note: <code>-u</code> and <code>--set-upstream</code> do the same thing
       </Section>
     </div>
   );

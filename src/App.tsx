9b5a9c54afbdcc2218a96df46a2e21f174c2fa85
urboypx <guilhermeciotapx@outlook.com>
2022-10-11 07:17:01 -0300

    feat: presentation section

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,25 @@ import Code from "./components/Code/Code";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Section id={0} title={t.init_title}>
+      <Section id={0} title={t.presentation_title}>
+        {t.presentation_desc} <br />
+        <br />
+        {t.by}{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/itispx">
+          Px
+        </a>
+        <br />
+        <br />
+      </Section>
+
+      <Section id={1} title={t.init_title}>
         {t.init_repo} <br />
         <Code>git init</Code>
         {t.connect_repo_remote} <br />
         <Code>git remote add origin &lt;remote-repo-url&gt;</Code>
       </Section>
 
-      <Section id={1} title={t.clone_repo_title}>
+      <Section id={2} title={t.clone_repo_title}>
         {t.clone_local_repo} <br />
         <Code>git clone /path/to/repo</Code>
         {t.clone_remote_repo} <br />
@@ -25,7 +36,7 @@ const App: React.FC = () => {
         <Code>gh repo clone &lt;username&gt;/&lt;repository&gt;</Code>
       </Section>
 
-      <Section id={2} title={t.staging_title}>
+      <Section id={3} title={t.staging_title}>
         {t.staging_file} <br />
         <Code>git add &lt;file&gt;</Code>
         {t.staging_all} <br />
@@ -35,7 +46,7 @@ const App: React.FC = () => {
         <Code>git commit -m "descriptive commit message"</Code>
       </Section>
 
-      <Section id={3} title={t.push_title}>
+      <Section id={4} title={t.push_title}>
         {t.push_to_remote} <br />
         <Code>git push -u origin master</Code>
         {t.push_to_remote_desc} <br />
@@ -51,12 +62,12 @@ const App: React.FC = () => {
         <Code inline>--set-upstream</Code> {t.set_upstream_note_2}
       </Section>
 
-      <Section id={4} title={t.pull_title}>
+      <Section id={5} title={t.pull_title}>
         {t.pull_repo} <br />
         <Code>git pull</Code>
       </Section>
 
-      <Section id={5} title={t.branch_title}>
+      <Section id={6} title={t.branch_title}>
         {t.branch_create} <br />
         <Code>git branch &lt;branch&gt;</Code>
         {t.branch_switch} <br />
@@ -71,7 +82,7 @@ const App: React.FC = () => {
         <Code>git push -u origin &lt;branch&gt;</Code>
       </Section>
 
-      <Section id={6} title={t.merge_title}>
+      <Section id={7} title={t.merge_title}>
         {t.merge_to_active} <br />
         <Code>git merge &lt;branch&gt;</Code>
         {t.merge_conflict_1}
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import "./Section.scss";
 
 export interface SectionProps {
+  style?: CSSProperties;
   id: number;
   title: string;
   children: React.ReactNode;
 }
 
-const Section: React.FC<SectionProps> = ({ id, title, children }) => {
+const Section: React.FC<SectionProps> = ({ style, id, title, children }) => {
   return (
-    <section className={`Section ${id % 2 === 0 ? "Light" : "Dark"}`}>
+    <section style={style} className={`Section ${id % 2 === 0 ? "Light" : "Dark"}`}>
       <h1>{title}</h1>
       <br />
       <p>{children}</p>

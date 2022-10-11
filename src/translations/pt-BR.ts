c2591c7ba7fec4eacdc7ff6a8b6751eb878f06bd
urboypx <guilhermeciotapx@outlook.com>
2022-10-11 07:19:10 -0300

    feat: pt-BR translation

---
+++ src/translations/pt-BR.ts
@@ -0,0 +1,46 @@
+export const pt_BR = {
+  language: "Português",
+  by: "por",
+  presentation_title: "Folha de Cola Git",
+  presentation_desc: "Uma folha de cola para os comandos mais usados do git",
+  init_title: "criando um repositório novo",
+  init_repo: "para inicializar um repositório git dentro de uma pasta use",
+  connect_repo_remote: "para conectar seu repositório local a um repositório remoto use",
+  clone_repo_title: "clonando um repositório",
+  clone_local_repo: "para clonar um repositório local use",
+  clone_remote_repo: "para clonar um repositório remoto use",
+  clone_remote_repo_gh_cli: "para clonar um repositório remoto usando a CLI do GitHub",
+  staging_title: "staging (add) & commit",
+  staging_file:
+    "após fazer mudanças em um arquivo, você pode pôr essas mundanças em uma área temporária (staging) com",
+  staging_all:
+    "para pôr todas as mudanças que você fez no seu diretório atual para baixo na área temporária, use",
+  staging_commit:
+    "para fazer commit dos arquivos que você pos na área temporária para seu repositório remoto use",
+  push_title: "push",
+  push_to_remote: "para enviar seus commits ao seu repositório remoto use",
+  push_to_remote_desc:
+    "isso assume que você está enviando seus commits para a branch 'master'",
+  git_push: "a próxima vez que você usar 'push' você precisa usar apenas",
+  u_note_1: "a",
+  u_note_2: "tag deixa você definir qual será a branch padrão para seus commits",
+  push_different_branch: "se você quiser enviar seu commit para uma branch diferente use",
+  set_upstream_note_1: "e",
+  set_upstream_note_2: "fazem a mesma coisa",
+  pull_title: "pull",
+  pull_repo: "para trazer as últimas mudanças do seu repositório remoto use",
+  branch_title: "branching",
+  branch_create: "para criar uma nova branch use",
+  branch_switch: "para selecionar e usar a branch use",
+  branch_create_switch: "uma maneira abreviada de fazer isso é usando",
+  branch_create_switch_desc: "isso irá criar a branch e selecioná-la",
+  branch_create_switch_rem:
+    "não se esqueça de fazer push na sua branch para seu repositório remoto para outros poderem acessá-la",
+  merge_title: "merge",
+  merge_to_active: "para fazer um merge de uma branch com a sua branch selecionada use",
+  merge_conflict_1:
+    "no caso de um conflito você irá precisar resolve-los manualmente e adicioná-los com",
+  merge_conflict_2: "antes de fazer a merge das branch's",
+  merge_diff: "para ver a diferença entre as duas branch's use",
+  note: "nota:",
+};
